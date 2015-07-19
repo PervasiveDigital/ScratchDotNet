abe89c1bf4c23560528ac0ba637d91e96ca51f49
Martin Calsyn <martin@pervasive.digital>
2015-07-19 17:44:39 +0300

    GPLv3 License The switch to CC BY-SA 4.0 was a mistake. That license is not appropriate for software and even Creative Commons clearly states that, so GPLv3 will preserve our intent to keep this code strictly open-source.  Some files have alternative licensing and are so marked.

--- README.md
+++ README.md
@@ -9,14 +9,11 @@ The Scratch for .Net Micro Framework software was created by Pervasive Digital L
 tool for advancing the mission of MIT Scratch to enhance education in science, techechnology, 
 engineering and mathematics.
 
-Pull requests and issues are warmly welcomed. To be accepted, pull requests must be submitted under the Creative
-Commons Attribution-ShareAlike 4.0 International License.
+Pull requests and issues are warmly welcomed. The contents of pull requests must be compatible with licensing under GPLv3.
 
 ##License
 
-This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
-http://creativecommons.org/licenses/by-sa/4.0/
+This work is licensed under the GPL v3.  A copy is available in the LICENSE file and at http://www.gnu.org/licenses/gpl.txt
 
 A few files, adopted from other sources, may have different license terms and these files contain a license statement
-at the top of the file.
-
+at the top of the file. The original license and copyright on these files is not affected by their inclusion here.
--- src/netmf/Desktop/ScratchDeploymentHelper/Firmata/CircularBuffer.cs
+++ src/netmf/Desktop/ScratchDeploymentHelper/Firmata/CircularBuffer.cs
@@ -1,11 +1,28 @@
-﻿//-----------------------------------------------------------
+﻿//-------------------------------------------------------------------------
 //  (c) 2015 Pervasive Digital LLC
 //
-// This work is licensed under the Creative Commons 
-//    Attribution-ShareAlike 4.0 International License.
-// http://creativecommons.org/licenses/by-sa/4.0/
+//  This file is part of Scratch for .Net Micro Framework
 //
-//-----------------------------------------------------------
+//  "Scratch for .Net Micro Framework" is free software: you can 
+//  redistribute it and/or modify it under the terms of the 
+//  GNU General Public License as published by the Free Software 
+//  Foundation, either version 3 of the License, or (at your option) 
+//  any later version.
+//
+//  "Scratch for .Net Micro Framework" is distributed in the hope that
+//  it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+//  the GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with "Scratch for .Net Micro Framework". If not, 
+//  see <http://www.gnu.org/licenses/>.
+//
+//  This file has also been distributed previously under an Apache 2.0
+//  license and you may elect to use that license with this file. This 
+//  does not affect the licensing of any other file in Scratch for
+//  .Net Micro Framework.
+//-------------------------------------------------------------------------
 using System;
 using System.Collections;
 using System.Collections.Generic;
