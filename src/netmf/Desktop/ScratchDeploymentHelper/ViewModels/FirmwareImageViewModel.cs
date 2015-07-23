f807bb339676b1e1b77194d4d4e51c728b9975bb
Martin Calsyn <martin@pervasive.digital>
2015-07-23 22:43:12 +0300

    Fix headers

--- src/netmf/Desktop/ScratchDeploymentHelper/ViewModels/FirmwareImageViewModel.cs
+++ src/netmf/Desktop/ScratchDeploymentHelper/ViewModels/FirmwareImageViewModel.cs
@@ -1,12 +1,34 @@
-﻿using PervasiveDigital.Scratch.Common;
-using PervasiveDigital.Scratch.DeploymentHelper.Common;
-using PervasiveDigital.Scratch.DeploymentHelper.Models;
+﻿//-------------------------------------------------------------------------
+//  (c) 2015 Pervasive Digital LLC
+//
+//  This file is part of Scratch for .Net Micro Framework
+//
+//  "Scratch for .Net Micro Framework" is free software: you can 
+//  redistribute it and/or modify it under the terms of the 
+//  GNU General Public License as published by the Free Software 
+//  Foundation, either version 3 of the License, or (at your option) 
+//  any later version.
+//
+//  "Scratch for .Net Micro Framework" is distributed in the hope that
+//  it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+//  the GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with "Scratch for .Net Micro Framework". If not, 
+//  see <http://www.gnu.org/licenses/>.
+//
+//-------------------------------------------------------------------------
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Threading;
+
+using PervasiveDigital.Scratch.Common;
+using PervasiveDigital.Scratch.DeploymentHelper.Common;
+using PervasiveDigital.Scratch.DeploymentHelper.Models;
 
 namespace PervasiveDigital.Scratch.DeploymentHelper.ViewModels
 {
