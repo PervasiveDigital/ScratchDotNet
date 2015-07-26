7e94f4f672f9485644f0da3a4f2fe8f79dcef16f
Martin Calsyn <martin@pervasive.digital>
2015-07-26 15:19:18 +0300

    missing files added

---
+++ src/netmf/Desktop/Extensibility/AddIns/GadgeteerConfigAddIn/GadgeteerFirmwareConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.AddIn;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PervasiveDigital.Scratch.DeploymentHelper.Extensibility.AddInViews;
+
+namespace PervasigeDigital.Scratch.AddIn.GadgeteerConfigAddIn
+{
+    [AddIn("Gadgeteer Configuration AddIn", Version = "1.0.0.0")]
+    public class GadgeteerFirmwareConfig : IFirmwareConfiguration
+    {
+        public void Configure()
+        {
+        }
+    }
+}
---
+++ src/netmf/Desktop/Extensibility/HostSideAdapters/FirmwareConfigurationContractToViewHostSideAdapter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.AddIn.Pipeline;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PervasiveDigital.Scratch.DeploymentHelper.Extensibility.Contracts;
+using PervasiveDigital.Scratch.DeploymentHelper.Extensibility.HostViewAdapters;
+
+namespace PervasiveDigital.Scratch.DeploymentHelper.Extensibility.HostSideAdapters
+{
+    public class FirmwareConfigurationContractToViewHostSideAdapter
+        : IFirmwareConfiguration
+    {
+        private IFirmwareConfigurationContract _contract;
+        private System.AddIn.Pipeline.ContractHandle _handle;
+
+        public FirmwareConfigurationContractToViewHostSideAdapter(IFirmwareConfigurationContract contract) 
+        {
+            _contract = contract;
+            _handle = new ContractHandle(contract);
+        }
+
+        public void Configure()
+        {
+            _contract.Configure();
+        }
+    }
+}
